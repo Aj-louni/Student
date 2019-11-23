a5c60f327a231212826df6ca91c35630f2c72841
Rakan <rakan.ajlouni@gmail.com>
2019-11-23 13:48:01 +0200

    Third Test Done

--- app/src/main/java/com/rakanajlouni/student/views/fragments/UploadProjects.kt
+++ app/src/main/java/com/rakanajlouni/student/views/fragments/UploadProjects.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 
 import com.rakanajlouni.student.R
 
@@ -14,16 +15,35 @@ import com.rakanajlouni.student.R
  */
 class UploadProjects : Fragment() {
 
+    val types = arrayOf("IT", "Art")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+            var view= inflater.inflate(R.layout.fragment_upload_projects, container, false)
+            val spinner = view.findViewById<Spinner>(R.id.spinner)
+            spinner?.adapter = ArrayAdapter(activity?.applicationContext, R.layout.support_simple_spinner_dropdown_item, types)
+            spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    println("Error")
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val type = parent?.getItemAtPosition(position).toString()
+                    Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
+                    println(type)
+                }
+
+            }
+
         // Inflate the layout for this fragment
-        var view= inflater.inflate(R.layout.fragment_upload_projects, container, false)
         if (container != null) {
             container.removeAllViews();
         }
         return view
+
     }
 
 
